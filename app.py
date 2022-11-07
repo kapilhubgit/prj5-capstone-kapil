3694486add41e6e6bb17d87681690b88079071ee
Kapil Chaudhary <kapillearning@gmail.com>
2022-11-07 21:58:17 +0530

    Capstone Prj 5

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return """
-	       <h1 style='color: blue;'>This is my capstone project version 1 -- Kapil Chaudhary</h1>
+	       <h1 style='color: green;'>This is my capstone project version 2 -- Kapil Chaudhary</h1>
            """
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
